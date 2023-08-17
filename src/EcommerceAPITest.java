97776244852eafe938cf29c936592cecdf2d8423
Sourabh Sahil <sahilsourabh328@gmail.com>
2023-08-18 00:13:29 +0530

    Updated EcommerceAPITest

--- src/EcommerceAPITest.java
+++ src/EcommerceAPITest.java
@@ -1,5 +1,6 @@
-import io.restassured.builder.RequestSpecBuilder;
 import static io.restassured.RestAssured.given;
+
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import pojo.LoginRequest;
@@ -8,21 +9,20 @@ import pojo.LoginResponse;
 public class EcommerceAPITest {
 
 	public static void main(String[] args) {
-		
-		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+
+		RequestSpecification reqSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
 				.setContentType(ContentType.JSON).build();
-		
-		LoginRequest login = new LoginRequest();
-		login.setUserEmail("ace_kazuki@gmail.com");
-		login.setUserPassword("Acekazuki@123");
-		
-		LoginResponse response = given().log().all().spec(req).body(login)
-		.when().post("/api/ecom/auth/login")
-		.then().log().all().statusCode(200).extract().response().as(LoginResponse.class);
-		
-		System.out.println("token: "+response.getToken());
-		System.out.println("userId: "+response.getUserId());
-		System.out.println("message: "+response.getMessage());
+
+		LoginRequest loginRequest = new LoginRequest();
+		loginRequest.setUserEmail("ace_kazuki@gmail.com");
+		loginRequest.setUserPassword("Acekazuki@123");
+
+		LoginResponse loginResponse = given().log().all().spec(reqSpec).body(loginRequest).when().post("/api/ecom/auth/login")
+				.then().log().all().assertThat().statusCode(200).extract().response().as(LoginResponse.class);
+
+		System.out.println("token: " + loginResponse.getToken());
+		System.out.println("userId: " + loginResponse.getUserId());
+		System.out.println("message: " + loginResponse.getMessage());
 
 	}
 
